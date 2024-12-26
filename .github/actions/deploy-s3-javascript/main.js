2f75d15c9c0e14a1f731e575d373527c5c7a0374
Srinivasa Rao Bellamkonda <srenivas35@gmail.com>
2024-12-26 06:56:24 -0800

    custom github actions pushing website content to s3 bucket in aws

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,6 +2,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run(){
+    //Get Some input files
+    const bucket = core.getInput('bucket', { require: true });
+    const bucketRegion = core.getInput('bucket-region', { require: true });
+    const distFolder = core.getInput('dist-folder', { require: true });
+    
+    github.getOctokit
+
+    // Upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+    
     core.notice('Hello From My Custom java Script Actions!.')
 }
 
