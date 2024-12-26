cf82cd441653a4e781a2638a6ca5c7ad1143515d
Srinivasa Rao Bellamkonda <srenivas35@gmail.com>
2024-12-26 04:53:57 -0800

    github custom java \script action added

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core');
+const core = require('@actions/github');
+const core = require('@actions/exec');
+function run(){
+    core.notice('Hello From My Custom java Script Actions!.')
+}
+
+run();
